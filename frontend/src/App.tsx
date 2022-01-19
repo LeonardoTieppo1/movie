2ea2768cf54d5d637ad31c06d582093774f73194
LeonardoTieppo1 <leonardo.tieppo2@hotmail.com>
2022-01-19 20:54:27 -0300

    Criação do Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+//é assim que importa uma imagem no React para aproveitar em um componente proprio
+//colocando o ReactComponent entre chaves from 'caminho da imagem'
+//coloque dentro do link em forma de <tag />, pois o componente react na verdade vira uma sintaxe de <tag /> em html 
+
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1>Página Inicial</h1>
+      <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GhIcon} from 'assets/img/github1.svg';
+import './style.css';
+function Navbar(){
+    return(
+    <header>
+    <nav className="container">
+      <div className="movie-nav-conteudo">
+        <h1>Movies</h1>
+        <a href="https://github.com/LeonardoTieppo1/">
+          <div className="movie-contato-container">
+            <GhIcon/ >
+            <p className="movie-contato-link">/LeonardoTieppo1</p>
+          </div>
+          </a>
+      </div>
+    </nav>
+  </header>
+    );
+}
+
+export default Navbar;
