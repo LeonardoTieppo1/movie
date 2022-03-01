6a758c333713e591ab4e2b09390b2d84d56fbbd5
LeonardoTieppo1 <leonardo.tieppo2@hotmail.com>
2022-03-01 19:59:49 -0300

    Save,useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Movie, MoviePage } from 'types/movie';
+import { Link, useNavigate} from 'react-router-dom';
+import { Movie} from 'types/movie';
 import { BASE_URL } from 'utils/requests';
+import { validateEmail } from 'utils/validate';
 import './styles.css'; 
 
 type Props={
@@ -29,6 +30,8 @@ type Props={
 
     function FormCard({movieId}:Props) {
      
+        //esse objeto navigate serve para comando de direcionamento de rota 
+        const navigate=useNavigate();
 
         const[movie,setMovie]=useState<Movie>();
 
@@ -39,15 +42,72 @@ type Props={
             });
         },[movieId]);
 
+        //criar constante de envio de formulario com o uso do evento para executar alguma função
+        //para consertar o event deve colocar :React.FormEvent<HTMLFormElement> com isso deve consertar o tipo de evento do envio do formulario
+        //evitar que envie da tela, não recarregando a pagina e sim parar o envio e fazer de forma customizada 
+        //para parar o envio padrão coloque o event.preventDefault();
+        //para pegar o email deve colocar uma constante var= (event.target as any).email.value
+        //no typescript pode haver erros, essa propriedade values pode não existir para funcionar deve colocar 
+        //(event.target(referencia de formulario) as any).email(chamar no formulario para acessar o input do email).value(valor do input) 
+        //mesma coisa para o score 
+        //use o console para fazer testes sobre o envio dos dados do formulario 
+        //caso der certo vai aparecer no console sem erros e esta capturando os dados no formulario
+        //agora deve fazer um controle de formulario tanto elaborado, fazendo de forma simplificada 
+        //para isso deve fazer uma condição de escrever o email
+        /*     função de validação de email
 
+        const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}; 
 
+    se o email não for validado, ele vai barrar o envio do email de forma simples 
+    */
+        const handleSubmit=(event:React.FormEvent<HTMLFormElement>) => {
+            
+            event.preventDefault();
+        
+            const email= (event.target as any).email.value;
+            const score= (event.target as any).score.value;
+            
+
+            if(!validateEmail(email)){
+                 return;   
+            }
+
+            //configuração de requisição do Axios no uso do método PUT
+            //onde o baseURL:BASE_URL, vai receber a BASE_URL selecionada
+            //método de dados no caso será PUT
+            //a url selecionada será /scores
+            //e a data que é o corpo que será enviado os dados
+            const config: AxiosRequestConfig={
+                baseURL:BASE_URL,
+                method:'PUT',
+                url:'/scores',
+                data:{
+                    email: email,
+                    movieId:movieId,
+                    score: score
+                }
+            }
+            //fazer a requisição do PUT, usando o axios(config) vai resultar em uma promisse para executar alguma coisa
+            //na hora que voltar uma resposta tem que colocar .then
+            // quando chegar a resposta dar um console.log(response.data) para saber se está funcionando  
+            //caso der certo retire o console.log
+            axios(config).then(response => {
+                navigate("/");
+            });
+        }
 
          return (
              <div className="movie-form-container">
                  <img className="movie-imagem-poster" src={movie?.image} alt={movie?.title} />
                  <div className="movie-poster-bottom-container">
                      <h3>{movie?.title}</h3>
-                     <form className="movie-form">
+                     <form className="movie-form" onSubmit={handleSubmit}>
                          <div className="grupo-form movie-grupo-form">
                              <label htmlFor="email">Informe seu email</label>
                              <br/>
